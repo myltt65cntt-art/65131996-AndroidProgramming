5081cb1b6c0e7919b052a427daec663837e34bd4
La Thị Trà My <my.ltt.65cntt@ntu.edu.vn>
2026-04-20 06:07:51 +0700

    Hoàn Thành

--- KhoaTruoc/app/src/main/java/Thi/My65131996/khoatruoc/Cau3Fragment.java
+++ KhoaTruoc/app/src/main/java/Thi/My65131996/khoatruoc/Cau3Fragment.java
@@ -1,64 +1,52 @@
 package Thi.My65131996.khoatruoc;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Cau3Fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
 public class Cau3Fragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    LandScapeAdapter adapter;
+    ArrayList<LandScape>List;
+    RecyclerView recyclerViewLandScape;
     public Cau3Fragment() {
-        // Required empty public constructor
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Cau3ragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static Cau3Fragment newInstance(String param1, String param2) {
         Cau3Fragment fragment = new Cau3Fragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        List = new ArrayList<>();
+        List.add(new LandScape("eff","THÁP EFFEL",
+                "THÁP EFFEL"));
+        List.add(new LandScape("buckingham","buckingham",
+                "buckingham"));
+        List.add(new LandScape("lb","Lăng Bác",
+                "Lăng Bác"));
+        List.add(new LandScape("cchn","Tháp Hà Nội",
+                "Lăng Bác"));
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cau3ragment, container, false);
+
+        View viewCau3=  inflater.inflate(R.layout.fragment_cau3, container, false);
+        recyclerViewLandScape= viewCau3.findViewById(R.id.ryCau3);
+        RecyclerView.LayoutManager layoutLinear= new LinearLayoutManager(viewCau3.getContext());
+        recyclerViewLandScape.setLayoutManager(layoutLinear);
+
+        adapter= new LandScapeAdapter(viewCau3.getContext(),List);
+        recyclerViewLandScape.setAdapter(adapter);
+
+        return viewCau3;
     }
 }
